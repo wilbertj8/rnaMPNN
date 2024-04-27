cb6338d94c030bd454ff9654fc4629bab0dac416
Wilbert <wilbertjoseph08@gmail.com>
2024-04-27 18:35:31 -0400

    masking

--- training/preprocessing/create_pt_files.sh
+++ training/preprocessing/create_pt_files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --mem=32g
-#SBATCH -c 2
-#SBATCH --output=parse_multiple_chains.out
+#SBATCH -t 00:05:00
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH -J training
 
 source activate mlfold
 
-folder_with_pdbs="/Users/wilbertjoseph/Downloads/IW/solo_representative_pdb_all__3_326/"
+folder_with_pdbs="../../solo_representative_pdb_all__3_326/"
 
 python create_pt_files.py --input_path=$folder_with_pdbs --output_path='../dataset/pdb/'
